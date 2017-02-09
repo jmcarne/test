3e49484fc1a23ddf081c383e151b6e0ea7d50d22
josep.carne <josep.carne@scmspain.com>
2017-02-09 01:00:12 +0100

    Final commit

--- src/main/java/com/scmspain/middleware/framework/http/handle/HttpHandler.java
+++ src/main/java/com/scmspain/middleware/framework/http/handle/HttpHandler.java
@@ -2,9 +2,11 @@ package com.scmspain.middleware.framework.http.handle;
 
 import com.sun.net.httpserver.HttpExchange;
 
+import java.io.IOException;
+
 /**
  * Created by josep.carne on 05/02/2017.
  */
 public interface HttpHandler {
-    public void handle(HttpExchange httpExchange);
+    public void handle(HttpExchange httpExchange) throws IOException;
 }
